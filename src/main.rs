a2c5fba7a3adcd6b8309d74a24cdf9c3848486e4
pandaman64 <kointosudesuyo@infoseek.jp>
2016-04-29 23:47:00 +0900

    Implemented regex parsers.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::fmt;
-use std::io;
-use std::str;
 
 enum RegExpr{
     Character(char),
     Range(Vec<char>),
     Repeation(Box<RegExpr>),
     Branch(Box<RegExpr>,Box<RegExpr>),
-    Sequence(Vec<Box<RegExpr>>)
+    Sequence(Box<RegExpr>,Box<RegExpr>)
 }
 
 impl fmt::Debug for RegExpr{
@@ -17,7 +15,7 @@ impl fmt::Debug for RegExpr{
             RegExpr::Range(ref range) => write!(f, "({:?})",range),
             RegExpr::Repeation(ref expr) => write!(f, "({:?}*)", expr),
             RegExpr::Branch(ref lhs,ref rhs) => write!(f, "({:?}|{:?})", lhs, rhs),
-            RegExpr::Sequence(ref exprs) => write!(f, "({:?})", exprs)
+            RegExpr::Sequence(ref car,ref cdr) => write!(f, "({:?}{:?})", car, cdr)
         }
     }
 }
@@ -25,54 +23,89 @@ impl fmt::Debug for RegExpr{
 #[derive(Debug)]
 struct ParseError;
 
-fn character<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
-   match input.next(){
-       Some(c) => Ok(RegExpr::Character(c)),
-       None => Err(ParseError{}) 
-   }
-}
-
 fn range<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
-    unimplemented!()
+    let mut buffer = Vec::new();
+    loop{
+        match input.next(){
+            Some(']') => break,
+            Some(c) => buffer.push(c),
+            None => return Err(ParseError{})
+        }
+    }
+    Ok(RegExpr::Range(buffer))
 }
 
-fn repeation<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
-    unimplemented!()
+fn paren<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
+	let mut level = 0;
+	let mut buffer: Vec<char> = Vec::new();
+	loop{
+		match input.next(){
+			Some(')') => {
+				if level == 0 {
+					break;
+				}
+				else{
+					level -= 1
+				}
+			},
+			Some('(') => { 
+				level += 1
+			},
+			Some(c) => buffer.push(c),
+			None => return Err(ParseError)
+		}
+	}
+	parse(&mut buffer.into_iter())
 }
 
-fn branch<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
-    unimplemented!()
-}
-
-fn sequence<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
-    unimplemented!()
+fn parse_one<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
+	match input.next(){
+        Some('[') => range(input),
+		Some('(') => paren(input),
+        Some(c) if c != '*' && c != '|' => Ok(RegExpr::Character(c)),
+        _ => Err(ParseError{})
+    }
 }
 
 fn parse<T: Iterator<Item=char>>(input: &mut T) -> Result<RegExpr,ParseError>{
-    let mut input = input.peekable();
-    character(&mut input)
+	match parse_one(input){
+		e @ Err(_) => e,
+		Ok(expr) => {
+			match input.next(){
+				Some('|') => {
+					match parse(input){
+						Ok(rhs) => Ok(RegExpr::Branch(Box::new(expr),Box::new(rhs))),
+						e @ Err(_) => e
+					}
+				},
+				Some('*') => {
+					let expr = RegExpr::Repeation(Box::new(expr));
+					match input.next(){
+						Some(c) => {
+							match parse(&mut Some(c).into_iter().chain::<Box<Iterator<Item=char>>>(Box::new(input))){
+								Ok(rhs) => Ok(RegExpr::Sequence(Box::new(expr),Box::new(rhs))),
+								e @ Err(_) => e
+							}
+						},
+						None => Ok(expr) 
+					}
+				},
+				Some(c) => {
+					match parse(&mut Some(c).into_iter().chain::<Box<Iterator<Item=char>>>(Box::new(input))){
+						Ok(rhs) => Ok(RegExpr::Sequence(Box::new(expr),Box::new(rhs))),
+						e @ Err(_) => e
+					}
+				},
+				None => Ok(expr)
+			}
+		}
+	}
 }
 
 fn main() {
-    /*
-    let expr1 =
-        Box::new(RegExpr::Repeation(
-            Box::new(RegExpr::Branch(
-                Box::new(RegExpr::Character('a')),
-                Box::new(RegExpr::Range(vec!['A','B','C','Z']))
-            ))
-        ));
-    let expr2 =
-        Box::new(RegExpr::Branch(
-            Box::new(RegExpr::Character('b')),
-            Box::new(RegExpr::Character('B'))
-        ));
-    let expr = RegExpr::Sequence(vec![expr1,expr2]);
-    */
-    let mut input = String::new();
-    let expression = match io::stdin().read_line(&mut input){
-        Ok(_) => parse(&mut input.chars()),
-        Err(error) => panic!("err {}", error)
-    };
+    let input = "a*i(u|e)*o".to_owned();
+    let expression = parse(&mut input.chars());
+    
+	println!("{}",input);
     println!("{:?}",expression);
 }
